2f7851dcfe890bdf0c8ed9277df9816ff62a5f72
Aman Chaubey <amanchaubey86@gmail.com>
2024-06-11 18:40:34 +0530

    update description

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -8,7 +8,7 @@ function ProductDescription() {
     <Box display={'flex'} alignItems={'center'} flexDirection={'column'} justifyContent={'center'} textAlign="center" p={5}>
         <Heading fontSize={'5xl'}>Car Assembly</Heading>
       <Image w={'20%'} src="https://c1.wallpaperflare.com/preview/200/862/475/black-background-audi.jpg" alt="Car" />
-      <Text fontSize="xl" w={'40%'} mt={4}>This is a description of the car. It highlights the key features and components of the car.</Text>
+      <Text fontSize="xl" w={'40%'} mt={4}>A ReactJS web app guides users through car assembly: 1st screen introduces with image & description, 2nd allows part selection, 3rd enables drag-and-drop assembly, and 4th displays the final product for review. Smooth, intuitive experience ensured.</Text>
       <Button colorScheme="red" size="lg" mt={4} onClick={() => navigate('/parts-selection')}>START</Button>
     </Box>
   );
